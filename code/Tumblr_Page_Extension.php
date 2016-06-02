33ff1bc137327bd08599c91e87cf777fbb637ec9
tkiehne <kiehnet@netscape.net>
2016-06-02 14:02:32 -0700

    PSR-2

--- code/Tumblr_Page_Extension.php
+++ code/Tumblr_Page_Extension.php
@@ -29,7 +29,8 @@ class TumblrPageExtension extends Extension
      * @param  SiteConfig    $config    the current SS SiteConfig
      * @return boolean required config strings are present (true) or not (false)
      */
-    private function checkConfig($config) {
+    private function checkConfig($config) 
+    {
         return !empty($config->TumblrBlogName)
             && !empty($config->TumblrConsumerKey)
             && !empty($config->TumblrConsumerSecret);
@@ -41,11 +42,12 @@ class TumblrPageExtension extends Extension
      * @param  object    $obj    The object to iterate (stdClass)
      * @return ArrayList|ArrayData  Completed conversion
      */
-    protected static function recursive_conversion_iterator($obj) {
-        if(is_object($obj) || is_array($obj)) {   
+    protected static function recursive_conversion_iterator($obj) 
+    {
+        if (is_object($obj) || is_array($obj)) {   
             //Check for properties with non-object values
             $is_list = true;         
-            foreach($obj as &$item) {
+            foreach ($obj as &$item) {
                 if(is_object($item) || is_array($item))
                     $item = self::recursive_conversion_iterator($item);
                 else
@@ -53,7 +55,7 @@ class TumblrPageExtension extends Extension
             }
         }
         //Format parent
-        if($is_list || is_array($obj))
+        if ($is_list || is_array($obj))
             $obj = ArrayList::create((array) $obj);
         else
             $obj = ArrayData::create((array) $obj);
@@ -74,28 +76,23 @@ class TumblrPageExtension extends Extension
     {        
         $config = SiteConfig::current_site_config();
 
-        if (!$this->checkConfig($config)) 
-        {
+        if (!$this->checkConfig($config)) {
             return new ArrayList();
         }
-        else
-        {
+        else {
             $cache_key = __FUNCTION__.'_'.md5($limit.$offset.$type.implode($options));
 
             $cache = SS_Cache::factory('tumblr_api_cache');
             if (!($results = unserialize($cache->load($cache_key)))) {
                 $results = new ArrayList();
 
-                if(!empty($type) && defined('self::POST_TYPE_'.strtoupper($type)))
-                {
+                if (!empty($type) && defined('self::POST_TYPE_'.strtoupper($type))) {
                     $options[self::OPTION_TYPE] = $type;
                 }
-                if(is_numeric($limit) && $limit > 0 && $limit <= self::DEFAULT_LIMIT)
-                {
+                if (is_numeric($limit) && $limit > 0 && $limit <= self::DEFAULT_LIMIT) {
                     $options[self::OPTION_LIMIT] = $limit;
                 }
-                if(is_numeric($offset) && $offset > 0)
-                {
+                if (is_numeric($offset) && $offset > 0) {
                     $options[self::OPTION_OFFSET] = $offset;
                 }
 
@@ -103,12 +100,11 @@ class TumblrPageExtension extends Extension
                 
                 try {
                     $response = $client->getBlogPosts($config->TumblrBlogName, $options);
-                    foreach($response->posts as $post)
-                    {               
+                    foreach ($response->posts as $post) {               
                         $results->push(self::recursive_conversion_iterator($post));
                     }            
                 }
-                catch(RequestException $e) {
+                catch (RequestException $e) {
                     // *** maybe we should do something?
                 }
                 $cache->save(serialize($results), $cache_key);
@@ -130,13 +126,11 @@ class TumblrPageExtension extends Extension
     public function TumblrTextPostsList($limit = null, $offset = null, $tag = "", $text = false)
     {
         $options = array();
-        if(!empty($tag))
-        {
+        if (!empty($tag)) {
             // ***input validation
             $options[self::OPTION_TAG] = $tag;
         }
-        if($text)
-        {
+        if ($text) {
             $options[self::OPTION_FILTER] = self::OPTION_FILTER_TEXT;
         }
         return $this->TumblrPostsList($limit, $offset, self::POST_TYPE_TEXT, $options);
@@ -155,13 +149,11 @@ class TumblrPageExtension extends Extension
     public function TumblrQuotePostsList($limit = null, $offset = null, $tag = "", $text = false)
     {
         $options = array();
-        if(!empty($tag))
-        {
+        if (!empty($tag)) {
             // ***input validation
             $options[self::OPTION_TAG] = $tag;
         }
-        if($text)
-        {
+        if ($text) {
             $options[self::OPTION_FILTER] = self::OPTION_FILTER_TEXT;
         }
         return $this->TumblrPostsList($limit, $offset, self::POST_TYPE_QUOTE, $options);
@@ -180,13 +172,11 @@ class TumblrPageExtension extends Extension
     public function TumblrLinkPostsList($limit = null, $offset = null, $tag = "", $text = false)
     {
         $options = array();
-        if(!empty($tag))
-        {
+        if (!empty($tag)) {
             // ***input validation
             $options[self::OPTION_TAG] = $tag;
         }
-        if($text)
-        {
+        if ($text) {
             $options[self::OPTION_FILTER] = self::OPTION_FILTER_TEXT;
         }
         return $this->TumblrPostsList($limit, $offset, self::POST_TYPE_LINK, $options);
@@ -205,13 +195,11 @@ class TumblrPageExtension extends Extension
     public function TumblrAnswerPostsList($limit = null, $offset = null, $tag = "", $text = false)
     {
         $options = array();
-        if(!empty($tag))
-        {
+        if (!empty($tag)) {
             // ***input validation
             $options[self::OPTION_TAG] = $tag;
         }
-        if($text)
-        {
+        if ($text) {
             $options[self::OPTION_FILTER] = self::OPTION_FILTER_TEXT;
         }
         return $this->TumblrPostsList($limit, $offset, self::POST_TYPE_ANSWER, $options);
@@ -230,13 +218,11 @@ class TumblrPageExtension extends Extension
     public function TumblrVideoPostsList($limit = null, $offset = null, $tag = "", $text = false)
     {
         $options = array();
-        if(!empty($tag))
-        {
+        if (!empty($tag)) {
             // ***input validation
             $options[self::OPTION_TAG] = $tag;
         }
-        if($text)
-        {
+        if ($text) {
             $options[self::OPTION_FILTER] = self::OPTION_FILTER_TEXT;
         }
         return $this->TumblrPostsList($limit, $offset, self::POST_TYPE_VIDEO, $options);
@@ -255,13 +241,11 @@ class TumblrPageExtension extends Extension
     public function TumblrAudioPostsList($limit = null, $offset = null, $tag = "", $text = false)
     {
         $options = array();
-        if(!empty($tag))
-        {
+        if (!empty($tag)) {
             // ***input validation
             $options[self::OPTION_TAG] = $tag;
         }
-        if($text)
-        {
+        if ($text) {
             $options[self::OPTION_FILTER] = self::OPTION_FILTER_TEXT;
         }
         return $this->TumblrPostsList($limit, $offset, self::POST_TYPE_AUDIO, $options);
@@ -280,13 +264,11 @@ class TumblrPageExtension extends Extension
     public function TumblrPhotoPostsList($limit = null, $offset = null, $tag = "", $text = false)
     {
         $options = array();
-        if(!empty($tag))
-        {
+        if (!empty($tag)) {
             // ***input validation
             $options[self::OPTION_TAG] = $tag;
         }
-        if($text)
-        {
+        if ($text) {
             $options[self::OPTION_FILTER] = self::OPTION_FILTER_TEXT;
         }
         return $this->TumblrPostsList($limit, $offset, self::POST_TYPE_PHOTO, $options);
@@ -305,13 +287,11 @@ class TumblrPageExtension extends Extension
     public function TumblrChatPostsList($limit = null, $offset = null, $tag = "", $text = false)
     {
         $options = array();
-        if(!empty($tag))
-        {
+        if (!empty($tag)) {
             // ***input validation
             $options[self::OPTION_TAG] = $tag;
         }
-        if($text)
-        {
+        if ($text) {
             $options[self::OPTION_FILTER] = self::OPTION_FILTER_TEXT;
         }
         return $this->TumblrPostsList($limit, $offset, self::POST_TYPE_CHAT, $options);
@@ -328,13 +308,11 @@ class TumblrPageExtension extends Extension
     public function TumblrPost($id, $text = false)
     {
         $options = array();
-        if($id)
-        {
+        if ($id) {
             // ***input validation
             $options[self::OPTION_ID] = $id;
         }
-        if($text)
-        {
+        if ($text) {
             $options[self::OPTION_FILTER] = self::OPTION_FILTER_TEXT;
         }
         return $this->TumblrPostsList(null, null, "", $options);
--- code/Tumblr_SiteConfig_Extension.php
+++ code/Tumblr_SiteConfig_Extension.php
@@ -2,14 +2,14 @@
 class TumblrSiteConfigExtension extends DataExtension
 {
     private static $db = array(
-      'TumblrBlogName' => 'Varchar(64)',
-      'TumblrConsumerKey' => 'Varchar(100)',
-      'TumblrConsumerSecret' => 'Varchar(100)'
+        'TumblrBlogName' => 'Varchar(64)',
+        'TumblrConsumerKey' => 'Varchar(100)',
+        'TumblrConsumerSecret' => 'Varchar(100)'
     );
     public function updateCMSFields(FieldList $fields)
     {
-      $fields->addFieldToTab("Root.Tumblr", new TextField('TumblrBlogName', _t('Tumblr.TumblrBlogName', 'Blog Name')));
-      $fields->addFieldToTab("Root.Tumblr", new TextField('TumblrConsumerKey', _t('Tumblr.TumblrConsumerKey', 'OAuth Consumer Key')));
-      $fields->addFieldToTab("Root.Tumblr", new TextField('TumblrConsumerSecret', _t('Tumblr.TumblrConsumerSecret', 'Consumer Secret')));
+        $fields->addFieldToTab("Root.Tumblr", new TextField('TumblrBlogName', _t('Tumblr.TumblrBlogName', 'Blog Name')));
+        $fields->addFieldToTab("Root.Tumblr", new TextField('TumblrConsumerKey', _t('Tumblr.TumblrConsumerKey', 'OAuth Consumer Key')));
+        $fields->addFieldToTab("Root.Tumblr", new TextField('TumblrConsumerSecret', _t('Tumblr.TumblrConsumerSecret', 'Consumer Secret')));
     }
 }
