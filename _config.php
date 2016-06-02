1ed50c3ef44ac73c7f51207d6a40f8d717fc1e1b
tkiehne <kiehnet@netscape.net>
2016-06-02 10:51:08 -0700

    cache enabled; phpdoc on methods

--- _config.php
+++ _config.php
@@ -8,8 +8,8 @@
  * @package tumblrfeed
  */
 
-#SS_Cache::add_backend('cache.tumblr.api', 'File', array('cache_dir' => TEMP_FOLDER . DIRECTORY_SEPARATOR . 'cache'));
-#SS_Cache::set_cache_lifetime('cache.tumblr.api', 1800, 10);
-#SS_Cache::pick_backend('cache.tumblr.api', 'any', 10);
+SS_Cache::add_backend('cache.tumblr.api', 'File', array('cache_dir' => TEMP_FOLDER . DIRECTORY_SEPARATOR . 'cache'));
+SS_Cache::set_cache_lifetime('cache.tumblr.api', 1800, 10);
+SS_Cache::pick_backend('cache.tumblr.api', 'any', 10);
 
 Object::add_extension('SiteConfig', 'Tumblr_SiteConfig_Extension');
--- code/Tumblr_Page_Extension.php
+++ code/Tumblr_Page_Extension.php
@@ -1,7 +1,10 @@
 <?php
+/**
+ * A wrapper class for the Tumblr API
+ */
 class Tumblr_Page_Extension extends Extension
 {
-    const DEFAULT_LIMIT = 12;
+    const DEFAULT_LIMIT = 20; // current max per API
 
     const POST_TYPE_TEXT = "text";
     const POST_TYPE_QUOTE = "quote";
@@ -20,6 +23,12 @@ class Tumblr_Page_Extension extends Extension
     const OPTION_FILTER = "filter";
     const OPTION_FILTER_TEXT = "text";
 
+    /**
+     * Check for required config values
+     *
+     * @param  SiteConfig    $config    the current SS SiteConfig
+     * @return boolean required config strings are present (true) or not (false)
+     */
     private function CheckConfig($config) {
         return !empty($config->TumblrBlogName)
             && !empty($config->TumblrConsumerKey)
@@ -29,7 +38,7 @@ class Tumblr_Page_Extension extends Extension
     /**
      * Recursively converts nested objects and arrays to Silverstripe ArrayList/ArrayData
      *
-     * @param object    $obj    The object to iterate (stdClass)
+     * @param  object    $obj    The object to iterate (stdClass)
      * @return ArrayList|ArrayData  Completed conversion
      */
     protected static function recursive_conversion_iterator($obj) {
@@ -51,6 +60,16 @@ class Tumblr_Page_Extension extends Extension
         return $obj;
     }
 
+    /**
+     * Get posts for a given blog
+     *
+     * @param integer $limit   number of results to return, max DEFAULT_LIMIT
+     * @param integer $offset  number of beginning position for retrieval 
+     * @param string  $type    the type of post to retrieve, or blank for all
+     * @param array   $options associative array of API options
+     *
+     * @return StdClass containing json decoded API results
+     */
     public function TumblrPostsList($limit = self::DEFAULT_LIMIT, $offset = 0, $type = "", $options = array())
     {        
         $config = SiteConfig::current_site_config();
@@ -61,15 +80,17 @@ class Tumblr_Page_Extension extends Extension
         }
         else
         {
-            //$cache = SS_Cache::factory('Tumblr_cache');
-            //if (!($results = unserialize($cache->load(__FUNCTION__)))) {
+            $cache_key = __FUNCTION__.'_'.md5($limit.$offset.$type.implode($options));
+
+            $cache = SS_Cache::factory('cache.tumblr.api');
+            if (!($results = unserialize($cache->load($cache_key)))) {
                 $results = new ArrayList();
 
                 if(!empty($type) && defined('self::POST_TYPE_'.strtoupper($type)))
                 {
                     $options[self::OPTION_TYPE] = $type;
                 }
-                if(is_numeric($limit) && $limit > 0)
+                if(is_numeric($limit) && $limit > 0 && $limit <= DEFAULT_LIMIT)
                 {
                     $options[self::OPTION_LIMIT] = $limit;
                 }
@@ -80,19 +101,32 @@ class Tumblr_Page_Extension extends Extension
 
                 $client = new Tumblr\API\Client($config->TumblrConsumerKey, $config->TumblrConsumerSecret);
                 
-                # *** error/exception handling?
-                $response = $client->getBlogPosts($config->TumblrBlogName, $options);
-                foreach($response->posts as $post)
-                {               
-                    $results->push(self::recursive_conversion_iterator($post));
-                }            
-                # ***cache by parameters
-                //$cache->save(serialize($results), __FUNCTION__);
-            //}
+                try {
+                    $response = $client->getBlogPosts($config->TumblrBlogName, $options);
+                    foreach($response->posts as $post)
+                    {               
+                        $results->push(self::recursive_conversion_iterator($post));
+                    }            
+                }
+                catch(RequestException e) {
+                    // *** maybe we should do something?
+                }
+                $cache->save(serialize($results), $cache_key);
+            }
         }
         return $results;
     }
 
+    /**
+     * Get text posts for a given blog
+     *
+     * @param integer $limit   number of results to return, max DEFAULT_LIMIT
+     * @param integer $offset  number of beginning position for retrieval 
+     * @param string  $tag     single tag to filter by
+     * @param boolean $text    return html-formatted user strings (false) or plain text (true)
+     *
+     * @return StdClass containing json decoded API results
+     */
     public function TumblrTextPostsList($limit = null, $offset = null, $tag = "", $text = false)
     {
         $options = array();
@@ -108,6 +142,16 @@ class Tumblr_Page_Extension extends Extension
         return $this->TumblrPostsList($limit, $offset, self::POST_TYPE_TEXT, $options);
     }
 
+    /**
+     * Get quote posts for a given blog
+     *
+     * @param integer $limit   number of results to return, max DEFAULT_LIMIT
+     * @param integer $offset  number of beginning position for retrieval 
+     * @param string  $tag     single tag to filter by
+     * @param boolean $text    return html-formatted user strings (false) or plain text (true)
+     *
+     * @return StdClass containing json decoded API results
+     */
     public function TumblrQuotePostsList($limit = null, $offset = null, $tag = "", $text = false)
     {
         $options = array();
@@ -123,6 +167,16 @@ class Tumblr_Page_Extension extends Extension
         return $this->TumblrPostsList($limit, $offset, self::POST_TYPE_QUOTE, $options);
     }
 
+    /**
+     * Get link posts for a given blog
+     *
+     * @param integer $limit   number of results to return, max DEFAULT_LIMIT
+     * @param integer $offset  number of beginning position for retrieval 
+     * @param string  $tag     single tag to filter by
+     * @param boolean $text    return html-formatted user strings (false) or plain text (true)
+     *
+     * @return StdClass containing json decoded API results
+     */
     public function TumblrLinkPostsList($limit = null, $offset = null, $tag = "", $text = false)
     {
         $options = array();
@@ -138,6 +192,16 @@ class Tumblr_Page_Extension extends Extension
         return $this->TumblrPostsList($limit, $offset, self::POST_TYPE_LINK, $options);
     }
 
+    /**
+     * Get answer posts for a given blog
+     *
+     * @param integer $limit   number of results to return, max DEFAULT_LIMIT
+     * @param integer $offset  number of beginning position for retrieval 
+     * @param string  $tag     single tag to filter by
+     * @param boolean $text    return html-formatted user strings (false) or plain text (true)
+     *
+     * @return StdClass containing json decoded API results
+     */
     public function TumblrAnswerPostsList($limit = null, $offset = null, $tag = "", $text = false)
     {
         $options = array();
@@ -153,6 +217,16 @@ class Tumblr_Page_Extension extends Extension
         return $this->TumblrPostsList($limit, $offset, self::POST_TYPE_ANSWER, $options);
     }
 
+    /**
+     * Get video posts for a given blog
+     *
+     * @param integer $limit   number of results to return, max DEFAULT_LIMIT
+     * @param integer $offset  number of beginning position for retrieval 
+     * @param string  $tag     single tag to filter by
+     * @param boolean $text    return html-formatted user strings (false) or plain text (true)
+     *
+     * @return StdClass containing json decoded API results
+     */
     public function TumblrVideoPostsList($limit = null, $offset = null, $tag = "", $text = false)
     {
         $options = array();
@@ -168,6 +242,16 @@ class Tumblr_Page_Extension extends Extension
         return $this->TumblrPostsList($limit, $offset, self::POST_TYPE_VIDEO, $options);
     }
 
+    /**
+     * Get audio posts for a given blog
+     *
+     * @param integer $limit   number of results to return, max DEFAULT_LIMIT
+     * @param integer $offset  number of beginning position for retrieval 
+     * @param string  $tag     single tag to filter by
+     * @param boolean $text    return html-formatted user strings (false) or plain text (true)
+     *
+     * @return StdClass containing json decoded API results
+     */
     public function TumblrAudioPostsList($limit = null, $offset = null, $tag = "", $text = false)
     {
         $options = array();
@@ -183,6 +267,16 @@ class Tumblr_Page_Extension extends Extension
         return $this->TumblrPostsList($limit, $offset, self::POST_TYPE_AUDIO, $options);
     }
 
+    /**
+     * Get photo posts for a given blog
+     *
+     * @param integer $limit   number of results to return, max DEFAULT_LIMIT
+     * @param integer $offset  number of beginning position for retrieval 
+     * @param string  $tag     single tag to filter by
+     * @param boolean $text    return html-formatted user strings (false) or plain text (true)
+     *
+     * @return StdClass containing json decoded API results
+     */
     public function TumblrPhotoPostsList($limit = null, $offset = null, $tag = "", $text = false)
     {
         $options = array();
@@ -198,6 +292,16 @@ class Tumblr_Page_Extension extends Extension
         return $this->TumblrPostsList($limit, $offset, self::POST_TYPE_PHOTO, $options);
     }
 
+    /**
+     * Get chat posts for a given blog
+     *
+     * @param integer $limit   number of results to return, max DEFAULT_LIMIT
+     * @param integer $offset  number of beginning position for retrieval 
+     * @param string  $tag     single tag to filter by
+     * @param boolean $text    return html-formatted user strings (false) or plain text (true)
+     *
+     * @return StdClass containing json decoded API results
+     */
     public function TumblrChatPostsList($limit = null, $offset = null, $tag = "", $text = false)
     {
         $options = array();
@@ -213,6 +317,14 @@ class Tumblr_Page_Extension extends Extension
         return $this->TumblrPostsList($limit, $offset, self::POST_TYPE_CHAT, $options);
     }
 
+    /**
+     * Get a single post for a given blog
+     *
+     * @param integer $id   Tumblr ID of post
+     * @param boolean $text return html-formatted user strings (false) or plain text (true)
+     *
+     * @return StdClass containing json decoded API result
+     */
     public function TumblrPost($id, $text = false)
     {
         $options = array();
